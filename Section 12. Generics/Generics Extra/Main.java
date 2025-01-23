c9f0d2890a806c7a23d80d7b07d20b14fae4b895
Asgard09 <thanhphuc04nh@gmail.com>
2025-01-23 17:45:08 +0700

    Generic methods, wildcards, and type erasure

--- Section 12. Generics/Generics Extra/Main.java
+++ Section 12. Generics/Generics Extra/Main.java
@@ -12,20 +12,42 @@ public class Main {
         for (int i = 0; i < studentCount; i++) {
             students.add(new Student());
         }
-//        students.add(new LPAStudent());
+        students.add(new LPAStudent());
 //        printList(students);
+        printMoreList(students);
 
         List<LPAStudent> lpaStudents = new ArrayList<>();
         for (int i = 0; i < studentCount; i++) {
             lpaStudents.add(new LPAStudent());
         }
-        printList(lpaStudents);
+//        printList(lpaStudents);
+        printMoreList(lpaStudents);
+
+        testList(new ArrayList<String>(List.of("Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Judy")));
+        testList(new ArrayList<Integer>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
     }
 
-    public static void printList(List students) {
+    // Wildcard
+    public static void printMoreList(List<? extends Student> students) {
         for (var student : students) {
-            System.out.println(student);
+            System.out.println(student.getYearStarted() + " " + student);
         }
         System.out.println();
     }
+
+    public static void testList(List<?> list){
+        for(var element : list){
+            if (element instanceof String s)
+                System.out.println("String: " + s.toUpperCase());
+            else if (element instanceof Integer i)
+                System.out.println("Integer: " + i.floatValue());
+        }
+    }
+
+//    public static <T extends Student> void printList(List<T> students) {
+//        for (var student : students) {
+//            System.out.println(student.getYearStarted() + " " + student);
+//        }
+//        System.out.println();
+//    }
 }
